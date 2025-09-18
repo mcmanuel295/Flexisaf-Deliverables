5a59133286ed046383d05590ea8d354246e3ed7a
unknown <mcmanuel755@gmail.com>
2025-09-18 01:08:28 +0100

    task 1

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("hello", "world", "java", "programming","madam");
+        int count = genericCount(strings, s -> s.length() > 4);
+        int count1 = genericCount(strings, s -> s.contentEquals(new StringBuilder(s).reverse()));
+
+        System.out.println(count);
+        System.out.println(count1);
+    }
+
+
+    /*Write a generic method to count the number of elements in a collection that have a specific property */
+
+    public static <T>int  genericCount(Iterable<T> iterable ,Predicate<T> predicate){
+        int count =0;
+
+        for(T element :iterable){
+
+            if (predicate.test(element)){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+
+}
